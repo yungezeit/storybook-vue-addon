a3809312df837ff8b4542d5009b5583f020e376a
Charles Gruenais <charles.gruenais@gmail.com>
2025-04-10 02:29:33 +0200

    fix: remove test manager styles from build

--- src/manager.tsx
+++ src/manager.tsx
@@ -4,13 +4,7 @@ import { addons, types, useParameter } from 'storybook/internal/manager-api';
 import { Panel } from './components/Panel';
 import { Tool } from './components/Tool';
 import { ADDON_ID, PANEL_ID, TOOL_ID } from './constants';
-import themeDark from './theme-dark';
-import themeLight from './theme-light';
 
-const dark = true;
-
-const theme = dark ? themeDark : themeLight;
-addons.setConfig({ theme });
 addons.register(ADDON_ID, (api) => {
   addons.add(TOOL_ID, {
     type: types.TOOL,
