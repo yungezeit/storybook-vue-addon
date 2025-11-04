c2ff8998de16b2afa38ae78d216fb4b30180e403
Charles Gruenais <charles.gruenais@gmail.com>
2025-11-04 19:42:43 +0100

    chore: upgrade to storybook 10.x.x

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,10 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import typescriptPreset from '@yungezeit/eslint-typescript';
 
-export default [...typescriptPreset, { ignoreFiles: ['scripts'] }];
+export default [
+  ...typescriptPreset,
+  { ignoreFiles: ['scripts'] },
+  ...storybook.configs["flat/recommended"]
+];
--- src/stories/Input.stories.ts
+++ src/stories/Input.stories.ts
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/vue3';
+import type { Meta, StoryObj } from '@storybook/vue3-vite';
 
 import Input from './Input.vue';
 
